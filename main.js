5c0fc48182101e47d159617894c366f33d16f314
exploit-j <kkarismajh@gmail.com>
2022-03-16 13:32:25 +0900

    To-do리스트 기본마크업 및 메인JS 완성

---
+++ main.js
@@ -0,0 +1,108 @@
+let userInput = document.querySelector('.task-input')
+let addButton = document.querySelector('.add-button')
+let taskList = []
+let tabs = document.querySelectorAll('.task-nav')
+let mode = 'tab-all'
+let filterList = []
+
+addButton.addEventListener('click', addTask)
+userInput.addEventListener('keypress', function(e){
+  if(e.key === 'Enter'){
+    addTask()
+  }
+})
+for(let i=0; i<tabs.length; i++){
+  tabs[i].addEventListener('click', function(e){filter(e)})
+}
+
+function addTask(){
+  let task = {
+    id : randomIDGenerator(),
+    taskContent : userInput.value,
+    isComplete : false
+  }
+  taskList.push(task)
+  userInput.value = ''
+  filter()
+}
+
+function render(){
+  let result = ''
+  let list = []
+
+  if(mode == 'tab-all'){
+    list = taskList
+  } else {
+    list = filterList
+  }
+
+  for(let i = 0; i < list.length; i++){
+    if(list[i].isComplete){
+      result += `<div class="task">
+      <div class="task-done">${list[i].taskContent}</div>
+      <div>
+        <button onclick="toggleComplete('${list[i].id}')">check</button>
+        <button onclick="deleteTask('${list[i].id}')">delete</button>
+      </div>
+    </div>`
+    } else{
+      result += `<div class="task">
+      <div>${list[i].taskContent}</div>
+      <div>
+      <button onclick="toggleComplete('${list[i].id}')">check</button>
+      <button onclick="deleteTask('${list[i].id}')">delete</button>
+      </div>
+      </div>`
+    }
+  }
+  document.querySelector('.task-board').innerHTML = result
+}
+
+function toggleComplete(toggleId){
+  for(let i=0; i<taskList.length; i++){
+    if(taskList[i].id == toggleId){
+      taskList[i].isComplete = !taskList[i].isComplete
+      break
+    }
+  }
+  filter()
+}
+
+function deleteTask(dtoggleId){
+  for(let i=0; i<taskList.length; i++){
+    if(taskList[i].id == dtoggleId){
+      taskList.splice(i, 1)
+      break
+    }
+  }
+  filter()
+}
+
+function filter(e){
+  if(e){
+    mode = e.target.id
+  }
+  
+  filterList = []
+  if(mode == 'tab-all'){
+    render()
+  }else if(mode == 'tab-not-done'){
+    for(let i=0; i<taskList.length; i++){
+      if(taskList[i].isComplete == false){
+        filterList.push(taskList[i])
+      }
+    }
+    render()
+  }else if(mode == 'tab-done'){
+    for(let i=0; i<taskList.length; i++){
+      if(taskList[i].isComplete == true){
+        filterList.push(taskList[i])
+      }
+    }
+    render()
+  }
+}
+
+function randomIDGenerator() {
+  return "_" + Math.random().toString(36).substring(2, 11);
+}
